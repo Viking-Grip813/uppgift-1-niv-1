1f34db4cc0802b913716f4530200362a1284ca1a
vist07001 <vist07001@E-PW05CGR8>
2024-12-11 12:06:05 +0100

    Add project files.

---
+++ uppgift 4.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+namespace uppggift1Nivå1
+{ class Program
+    { static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
